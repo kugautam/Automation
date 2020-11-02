421455a4dc0bbe2c88391ed87cb7f8416ebf2c56
kumargautam1162093@gmail.com <kumargautam1162093@gmail.com>
2020-11-02 12:25:56 +0530

    starter automation project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  public formData = {
+    uniqueNuber: '',
+    name: 'automation',
+    type:'Internal',
+    description:'nothing here',
+    comments:'nothing',
+    isr_ritm: 'BBW2'
+  };
+
+  public isUniqueNumberGenerated: boolean = false;
+
+
   title = 'Automation';
+
+  submit(){
+    this.generateUniqueNumber();
+    console.log(this.formData);
+    //api post .
+  }
+
+  generateUniqueNumber(){
+    //unique number generate
+    this.formData.uniqueNuber = `${this.formData.type}-Hello`;
+    this.isUniqueNumberGenerated = true;
+  }
 }
